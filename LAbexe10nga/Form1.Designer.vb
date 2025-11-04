c31c586d82d1ccb077deda8f1d2ea67544c2de1d
ReyAllenAntonio <ReyAllenAntonio@SMU-0121>
2025-11-04 09:38:37 +0800

    Add project files.

---
+++ LAbexe10nga/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.NumInput = New System.Windows.Forms.NumericUpDown()
+        Me.btnWrite = New System.Windows.Forms.Button()
+        Me.btnRead = New System.Windows.Forms.Button()
+        Me.Listbox1 = New System.Windows.Forms.ListBox()
+        Me.btnSort = New System.Windows.Forms.Button()
+        CType(Me.NumInput, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(79, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter  Number:"
+        '
+        'NumInput
+        '
+        Me.NumInput.Location = New System.Drawing.Point(163, 39)
+        Me.NumInput.Name = "NumInput"
+        Me.NumInput.Size = New System.Drawing.Size(155, 20)
+        Me.NumInput.TabIndex = 1
+        '
+        'btnWrite
+        '
+        Me.btnWrite.Location = New System.Drawing.Point(348, 40)
+        Me.btnWrite.Name = "btnWrite"
+        Me.btnWrite.Size = New System.Drawing.Size(132, 31)
+        Me.btnWrite.TabIndex = 2
+        Me.btnWrite.Text = "WRITE"
+        Me.btnWrite.UseVisualStyleBackColor = True
+        '
+        'btnRead
+        '
+        Me.btnRead.Location = New System.Drawing.Point(108, 115)
+        Me.btnRead.Name = "btnRead"
+        Me.btnRead.Size = New System.Drawing.Size(132, 31)
+        Me.btnRead.TabIndex = 3
+        Me.btnRead.Text = "READ"
+        Me.btnRead.UseVisualStyleBackColor = True
+        '
+        'Listbox1
+        '
+        Me.Listbox1.FormattingEnabled = True
+        Me.Listbox1.Location = New System.Drawing.Point(246, 88)
+        Me.Listbox1.Name = "Listbox1"
+        Me.Listbox1.Size = New System.Drawing.Size(234, 95)
+        Me.Listbox1.TabIndex = 4
+        '
+        'btnSort
+        '
+        Me.btnSort.Location = New System.Drawing.Point(348, 204)
+        Me.btnSort.Name = "btnSort"
+        Me.btnSort.Size = New System.Drawing.Size(132, 31)
+        Me.btnSort.TabIndex = 5
+        Me.btnSort.Text = "SORT"
+        Me.btnSort.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnSort)
+        Me.Controls.Add(Me.Listbox1)
+        Me.Controls.Add(Me.btnRead)
+        Me.Controls.Add(Me.btnWrite)
+        Me.Controls.Add(Me.NumInput)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = " "
+        CType(Me.NumInput, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents NumInput As NumericUpDown
+    Friend WithEvents btnWrite As Button
+    Friend WithEvents btnRead As Button
+    Friend WithEvents Listbox1 As ListBox
+    Friend WithEvents btnSort As Button
+End Class
---
+++ LAbexe10nga/Form1.vb
@@ -0,0 +1,80 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Public Class Form1
+
+        Dim filePath As String = "numbers.txt"
+        Dim numbers As New List(Of Integer)
+
+        ' WRITE button - add number to file
+        Private Sub btnWrite_Click(sender As Object, e As EventArgs) Handles btnWrite.Click
+            Dim num As Integer = CInt(NumInput.Value)
+
+            Using sw As New StreamWriter(filePath, True) ' True = append mode
+                sw.WriteLine(num)
+            End Using
+
+            MessageBox.Show("Number written to file successfully!", "WRITE", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            NumInput.Value = 0
+        End Sub
+
+        ' READ button - read numbers from file
+        Private Sub btnRead_Click(sender As Object, e As EventArgs) Handles btnRead.Click
+            ReadNumbersFromFile()
+        End Sub
+
+        ' SORT button - sort numbers, overwrite file, and auto-refresh display
+        Private Sub btnSort_Click(sender As Object, e As EventArgs) Handles btnSort.Click
+            If numbers.Count = 0 Then
+                MessageBox.Show("No numbers to sort. Please read from file first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                Return
+            End If
+
+            ' Sort numbers using LINQ OrderBy
+            Dim sorted = numbers.OrderBy(Function(n) n).ToList()
+
+            ' Overwrite the file with sorted numbers
+            Using sw As New StreamWriter(filePath, False) ' False = overwrite mode
+                For Each num In sorted
+                    sw.WriteLine(num)
+                Next
+            End Using
+
+            ' Automatically refresh the display with updated sorted list
+            numbers = sorted
+            Listbox1.Items.Clear()
+            For Each num In numbers
+                Listbox1.Items.Add(num)
+                Console.WriteLine(num)
+            Next
+
+            MessageBox.Show("Numbers sorted, saved, and refreshed successfully!", "SORT", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End Sub
+
+        ' Helper method to read numbers from file
+        Private Sub ReadNumbersFromFile()
+            If Not File.Exists(filePath) Then
+                MessageBox.Show("File not found! Please write numbers first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return
+            End If
+
+            Listbox1.Items.Clear()
+            numbers.Clear()
+
+            Using sr As New StreamReader(filePath)
+                While Not sr.EndOfStream
+                    Dim line As String = sr.ReadLine()
+                    Dim val As Integer
+                    If Integer.TryParse(line, val) Then
+                        numbers.Add(val)
+                        Listbox1.Items.Add(val)
+                    End If
+                End While
+            End Using
+
+            MessageBox.Show("Numbers read successfully!", "READ", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End Sub
+
+    End Class
+  
